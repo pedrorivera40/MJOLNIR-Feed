33a31ab35d5cf1a184fcb87712335ea64a7ab7b5
pedrorivera40 <pedro.rivera40@upr.edu>
2020-03-19 19:20:18 -0400

    still WIP fixing local packages issue

--- HeraclesAPI/config/dbconfig.py
+++ HeraclesAPI/config/dbconfig.py
@@ -2,10 +2,10 @@
 
 # Firebase RTBD Config.
 fb_config = {
-    apiKey: "AIzaSyAOGsd_Hwr8QXnEz6ArC21WOnhUPw_bJAE",
-    authDomain: "mjolnir-feed.firebaseapp.com",
-    databaseURL: "https://mjolnir-feed.firebaseio.com",
-    storageBucket: "mjolnir-feed.appspot.com",
+    'apiKey' : "AIzaSyAOGsd_Hwr8QXnEz6ArC21WOnhUPw_bJAE",
+    'authDomain' : "mjolnir-feed.firebaseapp.com",
+    'databaseURL' : "https://mjolnir-feed.firebaseio.com",
+    'storageBucket' : "mjolnir-feed.appspot.com",
 }
 
 
--- HeraclesAPI/dao/event.py
+++ HeraclesAPI/dao/event.py
@@ -1,11 +1,13 @@
-from firebase import firebase
+# from firebase import firebase
+import os
+CURRENT = os.path.dirname(os.path.abspath(__file__))
+PARENT = os.path.dirname(CURRENT)
 from config.dbconfig import fb_config
-
-firebase = Firebase(fb_config)
-rtdb = firebase.database()
-storage = firebase.storage()
-
+# firebase = Firebase(fb_config)
+# rtdb = firebase.database()
+# storage = firebase.storage()
 
 
-if __name__ == '__main__':
-    print(rtdb.v1.get())
+
+# if __name__ == '__main__':
+#     print(rtdb.v1.get())
