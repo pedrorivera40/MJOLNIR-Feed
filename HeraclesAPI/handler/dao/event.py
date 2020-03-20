2681cf89f1b832c7653ed144d18bf982c927583e
pedrorivera40 <pedro.rivera40@upr.edu>
2020-03-20 17:18:42 -0400

    fixing rtdb ref and adding comments functionality

--- HeraclesAPI/handler/dao/event.py
+++ HeraclesAPI/handler/dao/event.py
@@ -1,34 +1,64 @@
 from config.dbconfig import fb_config
 from firebase import Firebase
 
+''' 
+    Data Access Object implementation for the MJOLNIR-Feed prototype.
+    This class includes a set of methods to perform CRUD operations over
+    the Firebase RTDB instance regarding Events information.
+    @author pedrorivera40
+'''
+
 
 class EventDAO:
 
     def __init__(self):
         self.rtdb = Firebase(fb_config).database()
-        self.posts = self.rtdb.child("v1").child("posts")
 
+    # Retrieve events metadata to display in the Feed view.
     def get_events(self):
-        return self.posts.child("posts-metadata").get().val()
+        return self.rtdb.child("v1").child("posts").child("posts-metadata").get().val()
 
+    # Retrieve event content given its event_id.
     def get_event_by_id(self, event_id):
-        return self.posts.child("posts-content").child(event_id).get().val()
+        return self.rtdb.child("v1").child("posts").child("posts-content").child(event_id).get().val()
 
-    def create_event_content(self, post_content):
-        return self.posts.child("posts-content").push(post_content)
+    # Create new event given its content and metadata into a single dictionary.
+    def create_event(self, post):
+        event_info = self.rtdb.child("v1").child("posts").child(
+            "posts-content").push(post['metadata'])
+        return self.update_event_content(event_info['name'], post['content'])
 
-    def create_event_metadata(self, post_metadata):
-        return self.posts.child("posts-metadata").push(post_metadata)
-
+    # Update metadata of an event given its event_id.
     def update_event_metadata(self, event_id, new_metadata):
-        return self.posts.child("posts-metadata").child(event_id).set(new_metadata)
+        return self.rtdb.child("v1").child("posts").child("posts-metadata").child(event_id).set(new_metadata)
 
+    # Update content of an event given its event_id.
     def update_event_content(self, event_id, new_content):
-        return self.posts.child("posts-content").child(event_id).set(new_content)
+        return self.rtdb.child("v1").child("posts").child("posts-content").child(event_id).set(new_content)
 
-    # TODO -> Implement DELETE functions...
-    def fake_remove_event(self, event_id):
-        return self.posts.child(event_id).child("INVALID").set("true")
+    # Hide metadata of an event given its event_id.
+    def fake_remove_event_metadata(self, event_id):
+        return self.rtdb.child("v1").child("posts").child("posts-metadata").child(event_id).child("INVALID").set("true")
+
+    # Hide content of an event given its event_id.
+    def fake_remove_event_content(self, event_id):
+        return self.rtdb.child("v1").child("posts").child("posts-content").child(event_id).child("INVALID").set("true")
+
+    # Hide comments of an event given its event_id.
+    def fake_remove_event_comments(self, event_id):
+        return self.rtdb.child("v1").child("posts").child("comments").child(event_id).child("INVALID").set("true")
+
+    # Add a comment entry under an event.
+    def add_comment(self, event_id, comment):
+        return self.rtdb.child("v1").child("posts").child("comments").child(event_id).push(comment)
+
+    # Edit a comment entry under an event.
+    def edit_comment(self, event_id, comment_id, new_comment):
+        return self.rtdb.child("v1").child("posts").child("comments").child(event_id).child(comment_id).set(new_comment)
+
+    # Hide a comment entry under an event.
+    def fake_remove_comment_by_id(self, event_id, comment_id):
+        return self.rtdb.child("v1").child("posts").child("comments").child(event_id).child(comment_id).child("INVALID").set("true")
 
 
 if __name__ == '__main__':
