605c08717dee726cffeba479f365deed76bc098b
Onix <gotnix13@gmail.com>
2020-03-19 20:59:53 -0400

    Created login component and login page. Also created the default layout for when the user is authenticated

---
+++ ArtemisFrontEnd/pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <TheLoginForm />
+  </v-app>
+</template>
+
+
+<script>
+  import TheLoginForm from '~/components/TheLoginForm'
+  export default {
+    layout: "unauthenticated",
+    props: {
+      source: String
+    },
+    components: {
+      TheLoginForm,
+    },
+    data() {}
+  };
+</script>
+
+<style>
+</style>
