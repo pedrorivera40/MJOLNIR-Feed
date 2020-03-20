2857f57024c2d5dfcbc79db80115b2995f87300f
root <root@HerbertSP17.localdomain>
2020-03-19 20:33:08 -0400

    initial handler commit

--- HeraclesAPI/handler/event.py
+++ HeraclesAPI/handler/event.py
@@ -1,6 +1,21 @@
 from flask import jsonify
 
 class EventHandler:
-
+    def getAllEvents(self):
+        dao = EventsDAO()
+        events_list = dao.getAllEvents()
+        results_list = []
+        for row in events_list:
+            result = Event().build_dict_from_row(row)
+            result_list.append(result)
+        return jsonify(result_list)
+    
+    def getEventByID(self, eventId):
+        dao = dao.getEventbyId(eventId)
+        if not row:
+            return jsonify(Error = "Category Not Found"), 404
+        else: 
+            event = Event().build_dict_from_row(row)
+            return jsonify(category)
     
 
