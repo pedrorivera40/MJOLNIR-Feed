c7d93178987f000e69d8a12f06be97645a3a756e
pedrorivera40 <pedro.rivera40@upr.edu>
2020-03-20 20:14:38 -0400

    adding examples for using DAO + fix create_event

--- HeraclesAPI/handler/dao/event.py
+++ HeraclesAPI/handler/dao/event.py
@@ -25,7 +25,7 @@ class EventDAO:
     # Create new event given its content and metadata into a single dictionary.
     def create_event(self, post):
         event_info = self.rtdb.child("v1").child("posts").child(
-            "posts-content").push(post['metadata'])
+            "posts-metadata").push(post['metadata'])
         return self.update_event_content(event_info['name'], post['content'])
 
     # Update metadata of an event given its event_id.
@@ -67,3 +67,55 @@ if __name__ == '__main__':
 
     # Just checking if it works by doing a simple query...
     print(event_dao.get_events())
+
+'''
+# Examples for the remaining methods.
+
+# Get event content by id.
+event_id = 'post-id-1'
+print(event_dao.get_event_by_id(event_id))
+
+# Create evemt.
+new_event = {
+    'metadata': {
+        'author': 'user-2',
+        'date': '00009995',
+        'image-url': 'www.apicture.com',
+        'last-update': '00009995',
+        'summary': 'Compartiendo algunas frases celebres que todo el mundo deberia utilizar.',
+        'title': 'Posting an event for testing!',
+    },
+    'content': {
+        'author': 'user-2',
+        'date': '00009995',
+        'image-url': 'www.mysuperpicturewepa.com',
+        'last-update': '00009995',
+        'text': 'Frases celebres: La vida es complicada, es que no leen, eso me recuerda a mi GPA.',
+        'title': 'Posting an event for testing!',
+    },
+}
+print(event_dao.create_event(new_event))
+
+# Update metadata and content of an event.
+print(event_dao.update_event_metadata(event_id, new_event['metadata']))
+print(event_dao.update_event_content(event_id, new_event['content']))
+
+# Hide event content & metadata.
+print(event_dao.fake_remove_event_content(event_id))
+print(event_dao.fake_remove_event_metadata(event_id))
+
+# Hide comments.
+print(event_dao.fake_remove_event_comments(event_id))
+
+# Comments section.
+comment = {
+    'user': 'user-1',
+    'text': 'Again posting in my post! :P'
+}
+
+comment_info = event_dao.add_comment(event_id, comment)
+comment['text'] = 'NVM I won\'t comment again...'
+print(event_dao.edit_comment(event_id, comment_info['name'], comment))
+print(event_dao.fake_remove_comment_by_id(event_id, comment_info['name']))
+
+'''
